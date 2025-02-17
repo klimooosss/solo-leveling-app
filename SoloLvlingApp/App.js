c2d84c60537ec2458c5d7ac13ce7c02a8060484a
klimooosss <klimooss6@gmail.com>
2025-02-17 22:52:13 +0200

    added fonts and main label

--- SoloLvlingApp/App.js
+++ SoloLvlingApp/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <View style={[styles.container]}>
+      <Text style={[styles.text]}>Solo Leveling App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,9 +14,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#030212',
+  },
+  text: {
+    textAlign: 'center',
+    textAlignVertical: 'top',
+    color: '#fff',
+    paddingTop: 50,
+    fontSize: 25,
+    fontFamily: 'Poppins-Thin',
   },
 });
 
